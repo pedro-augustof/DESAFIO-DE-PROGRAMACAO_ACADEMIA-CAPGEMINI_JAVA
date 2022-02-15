1a50324291985c0481d9405427c0beb4377712ed
Pedro Augusto <pedro4032@hotmail.com>
2022-02-14 23:23:21 -0300

    Create Ex1.java

---
+++ Questões/Ex1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author pedro
+ */
+public class Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n, a, i, j, au;
+
+        System.out.printf("n=");
+        n = input.nextInt();
+
+        if (n <= 8 && n >= 1)
+        {
+            a = n;
+
+            for (i = 0; i < n; i++)
+            {
+                au = a - i - 1;
+
+                for (j = 0; j < au; j++)
+                {
+                    System.out.printf(" ");
+                }
+
+                for (j = au; j < n; j++)
+                {
+                    System.out.printf("*");
+                }
+                System.out.printf("\n");
+            }
+        }
+    }
+}
