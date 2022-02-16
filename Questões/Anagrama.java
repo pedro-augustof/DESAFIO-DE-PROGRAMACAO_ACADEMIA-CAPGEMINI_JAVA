2c4bc0cc6f0c1dc498c49f318168117e8fd5817d
Pedro Augusto <pedro4032@hotmail.com>
2022-02-16 11:37:21 -0300

    Update Anagrama.java
    Terminei a questão 3 de acordo com o que entendi.

--- Questões/Anagrama.java
+++ Questões/Anagrama.java
@@ -1,3 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author pedro
+ */
 public class Anagrama {
-    
+    public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+        int i, j, qtd, aux = 0;
+        char ci, cj;
+
+        String wd = input.nextLine().toLowerCase();
+        qtd = wd.length();
+        
+        for (i = 0; i < qtd; i++) {
+            ci = wd.charAt(i);
+
+            for (j = i + 1; j < qtd; j++) {
+                cj = wd.charAt(j);
+
+                if (ci == cj) {
+                    if (j - i > 1) {
+                        aux += 2;
+                    } else {
+                        aux +=1;
+                    }
+                }
+            }
+        }
+
+        System.out.println(aux);
+    }
 }
